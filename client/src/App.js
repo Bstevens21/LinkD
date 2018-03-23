3cbe388899234e73e7f61b78c15aec9306cb08b7
Blake Stevens <bwstevens@mavs.coloradomesa.edu>
2018-03-22 18:34:10 -0600

    proxy

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    response: ''
+  };
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.message }))
+      .catch(err => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch('/hello');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
   render() {
     return (
       <div className="App">
@@ -10,8 +24,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <p>{this.state.response}</p>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
