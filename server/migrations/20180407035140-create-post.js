0788007214cfcb13e558608edb4c9d2cfb4a4ea2
Daniel Powell <daniel@2kreate.com>
2018-04-16 19:05:29 -0600

    update other enum

---
+++ server/migrations/20180407035140-create-post.js
@@ -0,0 +1,61 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      startTime: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      duration: {
+        type: Sequelize.TIME,
+        allowNull: false,
+      },
+      location: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      requirements: {
+        type: Sequelize.STRING,
+      },
+      success: {
+        type: Sequelize.BOOLEAN,
+      },
+      expired: {
+        type: Sequelize.BOOLEAN,
+      },
+      notes: {
+        type: Sequelize.STRING,
+      },
+      body: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      category: {
+        type: Sequelize.ENUM,
+        values: ['sport','event','game','food','outdoor','club', 'other'],
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Posts');
+  }
+};
