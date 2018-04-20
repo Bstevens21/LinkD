1fb366aa95c1d19b316616c080cb1375e5cb8047
Blake Stevens <bwstevens@mavs.coloradomesa.edu>
2018-04-20 10:49:37 -0600

    intrested email

--- server/routes/index.js
+++ server/routes/index.js
@@ -25,6 +25,39 @@ module.exports = (app) => {
         res.json(updatedOwner);
       });
   });
+  app.get('/api/interested/:postTitle', function(req,res){
+    const random = require('randomstring');
+    const nodemailer = require('nodemailer');
+    
+    const postTitle = req.body.title;
+    
+    // create reusable transporter object using the default SMTP transport
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'linkdteam1',
+            pass: 'linkdpass21' 
+        }
+      });
+    
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: '"LinkD" <LinkDteam1@gmail.com>', // sender address
+        to: 'platinumwd1@gmail.com', // list of receivers
+        subject: 'Someone is interested in your activity', // Subject line
+        text: 'Blake Stevens is interested in attending your ' + postTitle + ' activity! Contact Blake at bwstevens@mavs.coloradomesa.edu.', // plaintext body
+        html: '<b></b>' // html body
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          return console.log(error);
+      }
+      console.log('Message sent: ' + info.response);
+  })
+     
+    
+  });
   app.post('/api/createPost', postController.create);
   app.post('/api/link', postController.link);
 };
